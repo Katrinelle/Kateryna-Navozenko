6b3f6a59b6cd837d2bea5ab0bd2d5a5eaf071cda
Kateryna Navozenko <katrinelle19@gmail.com>
2025-03-26 00:33:40 +0200

    English

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document
     event.preventDefault();
 
     let responseMessage = document.getElementById("response-message");
-    responseMessage.textContent = "Дякую! Ваше повідомлення надіслано.";
+    responseMessage.textContent = "Thank you! Your message was sended!";
     responseMessage.style.color = "green";
 
     setTimeout(() => (responseMessage.textContent = ""), 3000);
